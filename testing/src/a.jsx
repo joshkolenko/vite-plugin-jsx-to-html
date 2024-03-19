241e716e7b2e4e9fd357fc4c4d36b58e5cf0b3fd
Josh Kolenko <jkolenko+github@proton.me>
2024-03-19 16:03:02 -0400

    feat: update example

--- testing/src/a.jsx
+++ testing/src/a.jsx
@@ -5,6 +5,7 @@ import "./styles/styles.scss";
 import "./style2.css";
 
 import "./log";
+import "./data.js";
 
 export default function ComponentA() {
   return <Example>a.jsx</Example>;
---
+++ testing/src/data.js
@@ -0,0 +1,7 @@
+export default function () {
+  const data = {
+    name: "data.js",
+  };
+
+  console.log(data);
+}
